7828b7891b4cf2d6e2d6254b82ee125d54778372
assafBarash <assafbarash2@gmail.com>
2024-08-05 22:28:50 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+type BaseFunc<Params extends any[], Result> = (...params: Params) => Result
+
+export const CurryOn = <OgParams extends any[], OgResult>(
+    rootFunc: BaseFunc<OgParams, OgResult>
+) => {
+    const instance = (...params: OgParams) => rootFunc(...params)
+
+    instance.mapOutput = <NewResult>(
+        processOutputCb: BaseFunc<[OgResult], NewResult>
+    ) =>
+        CurryOn<OgParams, NewResult>((...params) => {
+            const result = rootFunc(...params)
+            return processOutputCb(result)
+        })
+
+    instance.mapInput = <NewParams extends any[]>(
+        processInputCb: BaseFunc<NewParams, OgParams>
+    ) =>
+        CurryOn<NewParams, OgResult>((...params) => {
+            const newParams = processInputCb(...params)
+            return rootFunc(...newParams)
+        })
+
+    return instance
+}
