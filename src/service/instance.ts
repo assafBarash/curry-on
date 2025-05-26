060f26dea6a6ab7108edd71f8212e04379344f1e
assafBarash <assaf@safebooks.ai>
2025-05-26 19:34:28 +0300

    fix(curry-on): types

--- src/service/instance.ts
+++ src/service/instance.ts
@@ -1,22 +1,23 @@
+import { BaseParams } from '@/types'
 import { createMapInput, createMapOutput } from './methods'
 
-type BaseFunc<Params extends any[], Result> = (...params: Params) => Result
+type BaseFunc<Params extends BaseParams, Result> = (...params: Params) => Result
 
 type Promisify<O, R> = O extends Promise<any> ? Promise<R> : R
 
-export type CurryOnInstance<OgParams extends any[], OgResult> = {
+export type CurryOnInstance<OgParams extends BaseParams, OgResult> = {
     (...params: OgParams): OgResult
     mapOutput: <NewResult>(
         cb: (result: Awaited<OgResult>, input: OgParams) => NewResult
     ) => CurryOnInstance<OgParams, Promisify<OgResult, NewResult>>
     mapOutputAsync: CurryOnInstance<OgParams, OgResult>['mapOutput']
-    mapInput: <NewParams extends any[]>(
+    mapInput: <NewParams extends BaseParams>(
         cb: (...params: NewParams) => OgParams
     ) => CurryOnInstance<NewParams, OgResult>
     clearCurryOn: () => BaseFunc<OgParams, OgResult>
 }
 
-export const CurryOn = <OgParams extends any[], OgResult>(
+export const CurryOn = <OgParams extends BaseParams, OgResult>(
     rootFunc: BaseFunc<OgParams, OgResult>
 ) => {
     const instance: CurryOnInstance<OgParams, OgResult> = (
--- src/service/methods/map-input.ts
+++ src/service/methods/map-input.ts
@@ -5,7 +5,7 @@ export const createMapInput =
     <OgParams extends BaseParams, OgResult>(
         rootFunc: BaseFunc<OgParams, OgResult>
     ) =>
-    <NewParams extends any[]>(mapInputCb: BaseFunc<NewParams, OgParams>) =>
+    <NewParams extends BaseParams>(mapInputCb: BaseFunc<NewParams, OgParams>) =>
         CurryOn<NewParams, OgResult>((...params) => {
             const newParams = mapInputCb(...params)
             return rootFunc(
